9e36c3efc9d3c515f6e74c67c4426e7e9a70bdb4
ragbinder <kozmary@gmail.com>
2014-03-22 21:38:24 -0700

    Test commit to master branch

--- RestaurantProblem/RPAppDelegate.h
+++ RestaurantProblem/RPAppDelegate.h
@@ -6,6 +6,14 @@
 //
 //
 
+/*
+This app has three screens that are all there to manage the data in a NSManagedObject held in the appDelegate. 
+ The first screen selects the date. 
+ The second screen selects a time and displays how many slots are available for each time slot. If there are no vacancies, then the second screen will not allow the user to proceed, and prompts them to select a different slot.
+ The third screen selects the number of guests, and then allows the user to attempt to confirm the reservation.
+ From here, the app will display a UIAlertView asking the user if they wish to confirm their currently selected reservation. If the user confirms, it will save the CoreData managed object context (held by the delegate) and return them to the first screen to make another reservation.
+*/
+
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 #import "RPAvailability.h"
